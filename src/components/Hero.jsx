3d21aafd23a733ffc93ac438cda9406e371a1bd7
freetimezp <kraftukrnet@ukr.net>
2023-12-23 17:36:37 +0200

    04. js-social-17-fashion. Hero Component.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useContext } from 'react';
 import './Hero.css';
 
@@ -8,8 +9,28 @@ function Hero() {
     //console.log(items, 'I here');
 
     return (
-        <div>
-
+        <div className='banner'>
+            {items && items.length > 0 && items.map((item) => (
+                <div className="item" key={item._id}>
+                    <img src={item.bgImg} alt="bg" className={`bgImg ${item.active ? 'active' : undefined}`} />
+                    <div className={`content ${item.active ? 'active' : undefined}`}>
+                        <p>{item.subtitle}</p>
+                        <h1>{item.title}</h1>
+                        <a href="#" className="mainButton">
+                            Shop Now <i className="bi bi-cart2"></i>
+                        </a>
+                        <a href="#" className="markButton">
+                            <i className="bi bi-bookmark-plus-fill"></i>
+                        </a>
+                    </div>
+                    <div className="subtitle">
+                        <span className="slogan">Spring & Summer Collections</span>
+                        <span className={`number ${item.active ? 'active' : undefined}`}>
+                            0{item._id}
+                        </span>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 };
